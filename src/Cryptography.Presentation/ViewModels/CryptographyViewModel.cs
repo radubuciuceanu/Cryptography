485c7cc5844a5e3348293c16402e79a1318b4de1
Radu B <radu.buciuceanu@gmail.com>
2018-11-20 03:11:57 +0200

    [feature-5] Cesar algorithm was implemented.

--- src/Cryptography.Presentation/ViewModels/CryptographyViewModel.cs
+++ src/Cryptography.Presentation/ViewModels/CryptographyViewModel.cs
@@ -1,19 +1,63 @@
-﻿using Caliburn.Micro;
+﻿using System.Reactive.Linq;
+using Caliburn.Micro;
+using Cryptography.Business.Commands;
+using Cryptography.Business.Commands.Contract;
+using Cryptography.Business.Models.Inputs;
 
 namespace Cryptography.Presentation.ViewModels
 {
 	public class CryptographyViewModel : PropertyChangedBase
 	{
-		public string Encrypted { get; set; }
+		private readonly IWindowManager windowManager;
+		private readonly ICesarAlgorithm cesarAlgorithm;
 
-		public string Decrypted { get; set; }
+		private string encrypted;
+		private string decrypted;
+
+		public string Encrypted
+		{
+			get => encrypted;
+			set
+			{
+				encrypted = value;
+				NotifyOfPropertyChange();
+				NotifyOfPropertyChange(() => CanCesarDecrypt);
+			}
+		}
+
+		public string Decrypted
+		{
+			get => decrypted;
+			set
+			{
+				decrypted = value;
+				NotifyOfPropertyChange();
+				NotifyOfPropertyChange(() => CanCesarEncrypt);
+			}
+		}
+
+		public bool CanCesarEncrypt => !string.IsNullOrWhiteSpace(Decrypted);
+
+		public bool CanCesarDecrypt => !string.IsNullOrWhiteSpace(Encrypted);
+
+		public CryptographyViewModel(IWindowManager windowManager)
+		{
+			this.windowManager = windowManager;
+			cesarAlgorithm = new CesarAlgorithm();
+		}
 
 		public void CesarEncrypt()
 		{
+			var viewModel = new CesarViewModel();
+			windowManager.ShowDialog(viewModel);
+			Encrypted = cesarAlgorithm.Execute(new CesarInput {Key = viewModel.Key, Value = Decrypted}).Wait();
 		}
 
 		public void CesarDecrypt()
 		{
+			var viewModel = new CesarViewModel();
+			windowManager.ShowDialog(viewModel);
+			Decrypted = cesarAlgorithm.Execute(new CesarInput {Key = -viewModel.Key, Value = Encrypted}).Wait();
 		}
 	}
 }
