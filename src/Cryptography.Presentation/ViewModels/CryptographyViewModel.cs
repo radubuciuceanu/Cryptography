67b0f824b3491d2886eebd54148afa2729d4dae3
Radu B <39530866+RaduBuciuceanu@users.noreply.github.com>
2018-11-20 02:11:30 +0200

    [feature-4] Layout for the main view was implemented. (#4)

--- src/Cryptography.Presentation/ViewModels/CryptographyViewModel.cs
+++ src/Cryptography.Presentation/ViewModels/CryptographyViewModel.cs
@@ -4,5 +4,16 @@ namespace Cryptography.Presentation.ViewModels
 {
 	public class CryptographyViewModel : PropertyChangedBase
 	{
+		public string Encrypted { get; set; }
+
+		public string Decrypted { get; set; }
+
+		public void CesarEncrypt()
+		{
+		}
+
+		public void CesarDecrypt()
+		{
+		}
 	}
 }
